aab3d413d36f2ad5559bc782bd6ee7bf42c4d910
Michael Stoneman <michaeljohnstoneman+github@gmail.com>
2026-02-13 17:13:16 -0500

    The necessary changes to get it published on gh pages.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 import CityPlanner from './city_planner.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
     <CityPlanner />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
